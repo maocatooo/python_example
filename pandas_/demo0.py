cb2063be73ef2016534f2b62f8baecd27d2f1bb6
maocat <zk246422@163.com>
2020-05-26 11:28:19 +0800

    fix

--- pandas_/demo0.py
+++ pandas_/demo0.py
@@ -4,13 +4,18 @@ import pandas as pd
 '''
 data = pd.read_excel('demo.xlsx', sheet_name=1)
 :arg
+    sheet_name=None 读取 excel 所有 sheet
     sheet_name=0 读取 excel 第一个sheet
     sheet_name=1 读取 excel 第二个sheet
     sheet_name >>> str 读取具体'sheet_name'的 sheet
-    
+    sheet_name >>> list [0, 1] 读取具体 多个 sheet
     不把第1行作为列名，读取Excel那就没有列名，需增加参数：header=None
 '''
-data = pd.read_excel('demo.xlsx', sheet_name=1)
+
+data = pd.read_excel('demo.xlsx', sheet_name=[0, 1], header=None)
+# print(data)
+# for i in data:
+#     print(data[i])
 '''
 # 获取最大行，最大列
 nrows = data.shape[0]
@@ -22,7 +27,7 @@ print(nrows, ncols)
 #显示列名，以列表形式显示
 print(data.columns)
 '''
-print(data.columns)
+# print(data.columns)
 
 # print(data)
 # print(data.head())
